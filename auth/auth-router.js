1288760668f3f6f32c6f284694a41f833bfee66e
Guillermo Alfaro <galfarotolon@gmail.com>
2020-06-22 11:36:03 -0500

    added knexfile setup, dbconfig

---
+++ auth/auth-router.js
@@ -0,0 +1,88 @@
+const bcryptjs = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+
+const router = require("express").Router();
+
+const Users = require("../users/users-model.js");
+
+
+const constants = require("../config/constants.js");
+
+
+
+
+router.post("/register", (req, res) => {
+    const credentials = req.body;
+
+    if (isValid(credentials)) {
+        const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+        const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+        credentials.password = hash;
+
+        Users.add(credentials)
+            .then(user => {
+                res.status(201).json({ data: user });
+            })
+            .catch(error => {
+                res.status(500).json({ message: error.message });
+            });
+    } else {
+        res.status(400).json({
+            message: "please provide username and password",
+        });
+    }
+});
+
+router.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    if (isValid(req.body)) {
+        Users.findBy({ username: username })
+            .then(([user]) => {
+                // compare the password the hash stored in the database
+                console.log("user", user);
+                if (user && bcryptjs.compareSync(password, user.password)) {
+                    const token = createToken(user);
+
+                    res.status(200).json({ token, message: "Welcome to the dad jokes API" });
+                } else {
+                    res.status(401).json({ message: "Invalid credentials" });
+                }
+            })
+            .catch(error => {
+                res.status(500).json({ message: error.message });
+            });
+    } else {
+        res.status(400).json({
+            message: "please provide username and password",
+        });
+    }
+});
+
+
+function isValid(user) {
+    return Boolean(user.username && user.password && typeof user.password === "string");
+}
+
+
+function createToken(user) {
+    const payload = {
+        subject: user.id,
+        name: user.name,
+        username: user.username,
+
+
+    }
+
+    const secret = constants.jwtSecret;
+
+    const options = {
+        expiresIn: "5d",
+    };
+
+    return jwt.sign(payload, secret, options);
+}
+
+module.exports = router;
--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,59 @@
-// Update with your config settings.
-
 module.exports = {
 
+  //development
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './database/recipesdb.db3'
     },
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     },
+    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './database/migrations',
+    },
+    seeds: { directory: './database/seeds' }
   },
 
+
+  //testing
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: { directory: './database/seeds' }
+  },
+
+  //production
   production: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './database/recipesdb.db3'
     },
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
     },
+    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './database/migrations',
+    },
+    seeds: { directory: './database/seeds' }
   }
 
 };
