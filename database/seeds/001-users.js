b5d1bb859251a047b98a2821d1915d3f69cd23af
Guillermo Alfaro <galfarotolon@gmail.com>
2020-06-25 15:32:20 -0500

    added more users as seed data

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -2,7 +2,10 @@ exports.seed = function (knex) {
   // only insert, cleanup.js is in charge of truncating the tables
   // trucating means deleting all records and resetting the primary key
   return knex("users").insert([
-    { first_name: "Guillermo", last_name: "Alfaro", username: "guillermo255", password: "123", email: "123@123.com" },
-    { first_name: "Barbara", last_name: "Moore", username: "Barbara123", password: "12345", email: "xyz@xyz.com" },
+    { first_name: "Guillermo", last_name: "Alfaro", username: "guillermo", password: "alfaro", email: "guillermo@secretfamilyrecipes.com" },
+    { first_name: "Barbara", last_name: "Moore", username: "barbara", password: "moore", email: "barbara@secretfamilyrecipes.com" },
+    { first_name: "Jane", last_name: "Bailey", username: "jane", password: "bailey", email: "jane@secretfamilyrecipes.com" },
+    { first_name: "Noah", last_name: "Green", username: "noah", password: "green", email: "noah@secretfamilyrecipes.com" },
+    { first_name: "Elijah", last_name: "Paminsan", username: "elijah", password: "paminsan", email: "elijah@secretfamilyrecipes.com" },
   ]);
 };
